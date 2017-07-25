09e98030f3ebe30c01bee90eac1209681b29f197
Timothy Anthony <pilot.taim@gmail.com>
2017-07-25 13:17:58 +0300

    Update and delete Region

--- resources/views/la/regions/edit.blade.php
+++ resources/views/la/regions/edit.blade.php
@@ -1,14 +1,14 @@
 @extends("la.layouts.app")
 
 @section("contentheader_title")
-	<a href="{{ url(config('laraadmin.adminRoute') . '/registrations') }}">Registration</a> :
+	<a href="{{ url(config('laraadmin.adminRoute') . '/regions') }}">Region</a> :
 @endsection
-@section("contentheader_description", $registration->$view_col)
-@section("section", "Registrations")
-@section("section_url", url(config('laraadmin.adminRoute') . '/registrations'))
+@section("contentheader_description", $region->$view_col)
+@section("section", "Regions")
+@section("section_url", url(config('laraadmin.adminRoute') . '/regions'))
 @section("sub_section", "Edit")
 
-@section("htmlheader_title", "Registrations Edit : ".$registration->$view_col)
+@section("htmlheader_title", "Regions Edit : ".$region->$view_col)
 
 @section("main-content")
 
@@ -29,21 +29,17 @@
 	<div class="box-body">
 		<div class="row">
 			<div class="col-md-8 col-md-offset-2">
-				{!! Form::model($registration, ['route' => [config('laraadmin.adminRoute') . '.registrations.update', $registration->id ], 'method'=>'PUT', 'id' => 'registration-edit-form']) !!}
+				{!! Form::model($region, ['route' => [config('laraadmin.adminRoute') . '.regions.update', $region->id ], 'method'=>'PUT', 'id' => 'region-edit-form']) !!}
 					@la_form($module)
 					
 					{{--
-					@la_input($module, 'status')
-					@la_input($module, 'firstname')
-					@la_input($module, 'middlename')
-					@la_input($module, 'email')
-					@la_input($module, 'surname')
-					@la_input($module, 'mobile')
-					@la_input($module, 'registration_reason')
+					@la_input($module, 'name')
+					@la_input($module, 'position')
+					@la_input($module, 'description')
 					--}}
                     <br>
 					<div class="form-group">
-						{!! Form::submit( 'Update', ['class'=>'btn btn-success']) !!} <button class="btn btn-default pull-right"><a href="{{ url(config('laraadmin.adminRoute') . '/registrations') }}">Cancel</a></button>
+						{!! Form::submit( 'Update', ['class'=>'btn btn-success']) !!} <button class="btn btn-default pull-right"><a href="{{ url(config('laraadmin.adminRoute') . '/regions') }}">Cancel</a></button>
 					</div>
 				{!! Form::close() !!}
 			</div>
@@ -56,7 +52,7 @@
 @push('scripts')
 <script>
 $(function () {
-	$("#registration-edit-form").validate({
+	$("#region-edit-form").validate({
 		
 	});
 });
