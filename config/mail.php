025edb0994ef85899f2cad27fd65f43cb73e4062
Timothy Anthony <pilot.taim@gmail.com>
2017-08-01 09:09:34 +0300

    Changing email host to mailgun

--- config/mail.php
+++ config/mail.php
@@ -29,7 +29,7 @@ return [
     |
     */
 
-    'host' => env('MAIL_HOST', 'smtp.gmail.com'),
+    'host' => env('MAIL_HOST', 'smtp.mailgun.org'),
 
     /*
     |--------------------------------------------------------------------------
