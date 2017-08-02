2b0b6d9eae76b8d70a0d3ee197fc925b62c7e7cc
Joseph Luvanda <lvandazz@gmail.com>
2017-08-02 13:10:54 +0300

    Dokku deployment

--- dwij1/laraadmin/src/Installs/resources/views/emails/verify.blade.php
+++
@@ -1,8 +0,0 @@
-Dear {{ $registration->firstname }},<br><br>
-
-You have requested access on {{ url('/') }}. Please click link below to verify your email address.<br><br>
-
-<a href={{ url('/email_verification/'.$registration->verification_token) }}>Verify Email</a><br><br>
-
-Best Regards,
-DMT - Karanga
--- dwij1/laraadmin/src/Installs/resources/views/la/layouts/partials/footer.blade.php
+++
@@ -1,8 +0,0 @@
-@if(!isset($no_padding))
-<footer class="main-footer">
-    <div class="pull-right hidden-xs">
-        A project by&nbsp;<a href="http://codefortanzania.org">Code For Tanzania</a>
-    </div>
-    <strong>&nbsp;
-</footer>
-@endif
