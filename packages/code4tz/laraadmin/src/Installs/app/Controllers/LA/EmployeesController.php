668017c9ff20e2dac28a829beb41128194b396e2
Joseph Luvanda <lvandazz@gmail.com>
2017-08-11 16:10:39 +0300

    Employee module updates

--- packages/code4tz/laraadmin/src/Installs/app/Controllers/LA/EmployeesController.php
+++ packages/code4tz/laraadmin/src/Installs/app/Controllers/LA/EmployeesController.php
@@ -17,7 +17,13 @@ use Collective\Html\FormFacade as Form;
 use Dwij\Laraadmin\Models\Module;
 use Dwij\Laraadmin\Models\ModuleFields;
 
+use App\User;
 use App\Models\Employee;
+use App\Role;
+use Mail;
+use Log;
+use Dwij\Laraadmin\Models\LAConfigs;
+use Dwij\Laraadmin\Helpers\LAHelper;
 
 class EmployeesController extends Controller
 {
@@ -85,7 +91,34 @@ class EmployeesController extends Controller
 				return redirect()->back()->withErrors($validator)->withInput();
 			}
 
-			$insert_id = Module::insert("Employees", $request);
+			// generate password
+			$password = LAHelper::gen_password();
+
+			// Create Employee
+			$employee_id = Module::insert("Employees", $request);
+			// Create User
+			$user = User::create([
+				'name' => $request->name,
+				'email' => $request->email,
+				'password' => bcrypt($password),
+				'context_id' => $employee_id,
+				'type' => "Employee",
+			]);
+
+			// update user role
+			$user->detachRoles();
+			$role = Role::find($request->role);
+			$user->attachRole($role);
+
+			if(env('MAIL_USERNAME') != null && env('MAIL_USERNAME') != "null" && env('MAIL_USERNAME') != "") {
+				// Send mail to User his Password
+				Mail::send('emails.send_login_cred', ['user' => $user, 'password' => $password], function ($m) use ($user) {
+					$m->from('joseph@codefortanzania.org', 'Admininistrator');
+					$m->to($user->email, $user->name)->subject('DMT Karanga - Your Login Credentials');
+				});
+			} else {
+				Log::info("User created: username: ".$user->email." Password: ".$password);
+			}
 
 			return redirect()->route(config('laraadmin.adminRoute') . '.employees.index');
 
@@ -108,11 +141,14 @@ class EmployeesController extends Controller
 			if(isset($employee->id)) {
 				$module = Module::get('Employees');
 				$module->row = $employee;
+				// Get User Table Information
+				$user = User::where('context_id', '=', $id)->firstOrFail();
 
 				return view('la.employees.show', [
 					'module' => $module,
-					'view_col' => $this->view_col,
+					'view_col' => $module->view_col,
 					'no_header' => true,
+					'user' => $user,
 					'no_padding' => "no-padding"
 				])->with('employee', $employee);
 			} else {
@@ -175,7 +211,17 @@ class EmployeesController extends Controller
 				return redirect()->back()->withErrors($validator)->withInput();;
 			}
 
-			$insert_id = Module::updateRow("Employees", $request, $id);
+			$employee_id = Module::updateRow("Employees", $request, $id);
+
+			// Update User
+			$user = User::where('context_id', $employee_id)->first();
+			$user->name = $request->name;
+			$user->save();
+
+			// update user role
+			$user->detachRoles();
+			$role = Role::find($request->role);
+			$user->attachRole($role);
 
 			return redirect()->route(config('laraadmin.adminRoute') . '.employees.index');
 
@@ -203,47 +249,86 @@ class EmployeesController extends Controller
 	}
 
 	/**
-	 * Datatable Ajax fetch
-	 *
-	 * @return
-	 */
-	public function dtajax()
-	{
-		$values = DB::table('employees')->select($this->listing_cols)->whereNull('deleted_at');
-		$out = Datatables::of($values)->make();
-		$data = $out->getData();
+ * Datatable Ajax fetch
+ *
+ * @return
+ */
+public function dtajax(Request $request)
+{
+	$module = Module::get('Employees');
+	$listing_cols = $this->listing_cols;
 
-		$fields_popup = ModuleFields::getModuleFields('Employees');
+	$values = DB::table('employees')->select($listing_cols)->whereNull('deleted_at');
+	$out = Datatables::of($values)->make();
+	$data = $out->getData();
 
-		for($i=0; $i < count($data->data); $i++) {
-			for ($j=0; $j < count($this->listing_cols); $j++) {
-				$col = $this->listing_cols[$j];
-				if($fields_popup[$col] != null && starts_with($fields_popup[$col]->popup_vals, "@")) {
-					$data->data[$i][$j] = ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$j]);
-				}
-				if($col == $this->view_col) {
-					$data->data[$i][$j] = '<a href="'.url(config('laraadmin.adminRoute') . '/employees/'.$data->data[$i][0]).'">'.$data->data[$i][$j].'</a>';
-				}
-				// else if($col == "author") {
-				//    $data->data[$i][$j];
-				// }
+	$fields_popup = ModuleFields::getModuleFields('Employees');
+
+	for($i=0; $i < count($data->data); $i++) {
+		for ($j=0; $j < count($listing_cols); $j++) {
+			$col = $listing_cols[$j];
+			if($fields_popup[$col] != null && starts_with($fields_popup[$col]->popup_vals, "@")) {
+				$data->data[$i][$j] = ModuleFields::getFieldValue($fields_popup[$col], $data->data[$i][$j]);
 			}
-
-			if($this->show_action) {
-				$output = '';
-				if(Module::hasAccess("Employees", "edit")) {
-					$output .= '<a href="'.url(config('laraadmin.adminRoute') . '/employees/'.$data->data[$i][0].'/edit').'" class="btn btn-warning btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-edit"></i></a>';
-				}
-
-				if(Module::hasAccess("Employees", "delete")) {
-					$output .= Form::open(['route' => [config('laraadmin.adminRoute') . '.employees.destroy', $data->data[$i][0]], 'method' => 'delete', 'style'=>'display:inline']);
-					$output .= ' <button class="btn btn-danger btn-xs" type="submit"><i class="fa fa-times"></i></button>';
-					$output .= Form::close();
-				}
-				$data->data[$i][] = (string)$output;
+			if($col == $module->view_col) {
+				$data->data[$i][$j] = '<a href="'.url(config('laraadmin.adminRoute') . '/employees/'.$data->data[$i][0]).'">'.$data->data[$i][$j].'</a>';
 			}
+			// else if($col == "author") {
+			//    $data->data[$i][$j];
+			// }
 		}
-		$out->setData($data);
-		return $out;
+
+		if($this->show_action) {
+			$output = '';
+			if(Module::hasAccess("Employees", "edit")) {
+				$output .= '<a href="'.url(config('laraadmin.adminRoute') . '/employees/'.$data->data[$i][0].'/edit').'" class="btn btn-warning btn-xs" style="display:inline;padding:2px 5px 3px 5px;"><i class="fa fa-edit"></i></a>';
+			}
+
+			if(Module::hasAccess("Employees", "delete")) {
+				$output .= Form::open(['route' => [config('laraadmin.adminRoute') . '.employees.destroy', $data->data[$i][0]], 'method' => 'delete', 'style'=>'display:inline']);
+				$output .= ' <button class="btn btn-danger btn-xs" type="submit"><i class="fa fa-times"></i></button>';
+				$output .= Form::close();
+			}
+			$data->data[$i][] = (string)$output;
+		}
+	}
+	$out->setData($data);
+	return $out;
+}
+/**
+     * Change Employee Password
+     *
+     * @return
+     */
+	public function change_password($id, Request $request) {
+
+		$validator = Validator::make($request->all(), [
+            'password' => 'required|min:6',
+			'password_confirmation' => 'required|min:6|same:password'
+        ]);
+
+		if ($validator->fails()) {
+			return \Redirect::to(config('laraadmin.adminRoute') . '/employees/'.$id)->withErrors($validator);
+		}
+
+		$employee = Employee::find($id);
+		$user = User::where("context_id", $employee->id)->where('type', 'Employee')->first();
+		$user->password = bcrypt($request->password);
+		$user->save();
+
+		\Session::flash('success_message', 'Password is successfully changed');
+
+		// Send mail to User his new Password
+		if(env('MAIL_USERNAME') != null && env('MAIL_USERNAME') != "null" && env('MAIL_USERNAME') != "") {
+			// Send mail to User his new Password
+			Mail::send('emails.send_login_cred_change', ['user' => $user, 'password' => $request->password], function ($m) use ($user) {
+				$m->from(LAConfigs::getByKey('default_email'), LAConfigs::getByKey('sitename'));
+				$m->to($user->email, $user->name)->subject('DMT Karanga - Login Credentials changed');
+			});
+		} else {
+			Log::info("User change_password: username: ".$user->email." Password: ".$request->password);
+		}
+
+		return redirect(config('laraadmin.adminRoute') . '/employees/'.$id.'#tab-account-settings');
 	}
 }
--- resources/views/la/employees/show.blade.php
+++ resources/views/la/employees/show.blade.php
@@ -39,6 +39,9 @@
 	<ul data-toggle="ajax-tab" class="nav nav-tabs profile" role="tablist">
 		<li class=""><a href="{{ url(config('laraadmin.adminRoute') . '/employees') }}" data-toggle="tooltip" data-placement="right" title="Back to Employees"><i class="fa fa-chevron-left"></i></a></li>
 		<li class="active"><a role="tab" data-toggle="tab" class="active" href="#tab-general-info" data-target="#tab-info"><i class="fa fa-bars"></i> General Info</a></li>
+		@if($employee->id == Auth::user()->id || Entrust::hasRole("SUPER_ADMIN"))
+	<li class=""><a role="tab" data-toggle="tab" href="#tab-account-settings" data-target="#tab-account-settings"><i class="fa fa-key"></i> Account settings</a></li>
+@endif
 	</ul>
 
 	<div class="tab-content">
@@ -70,6 +73,49 @@
 				</div>
 			</div>
 		</div>
+		@if($employee->id == Auth::user()->id || Entrust::hasRole("SUPER_ADMIN"))
+<div role="tabpanel" class="tab-pane fade" id="tab-account-settings">
+	<div class="tab-content">
+		<form action="{{ url(config('laraadmin.adminRoute') . '/change_password/'.$employee->id) }}" id="password-reset-form" class="general-form dashed-row white" method="post" accept-charset="utf-8">
+			{{ csrf_field() }}
+			<div class="panel">
+				<div class="panel-default panel-heading">
+					<h4>Account settings</h4>
+				</div>
+				<div class="panel-body">
+					@if (count($errors) > 0)
+						<div class="alert alert-danger">
+							<ul>
+								@foreach ($errors->all() as $error)
+									<li>{{ $error }}</li>
+								@endforeach
+							</ul>
+						</div>
+					@endif
+					@if(Session::has('success_message'))
+						<p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('success_message') }}</p>
+					@endif
+					<div class="form-group">
+						<label for="password" class=" col-md-2">Password</label>
+						<div class=" col-md-10">
+							<input type="password" name="password" value="" id="password" class="form-control" placeholder="Password" autocomplete="off" required="required" data-rule-minlength="6" data-msg-minlength="Please enter at least 6 characters.">
+						</div>
+					</div>
+					<div class="form-group">
+						<label for="password_confirmation" class=" col-md-2">Retype password</label>
+						<div class=" col-md-10">
+							<input type="password" name="password_confirmation" value="" id="password_confirmation" class="form-control" placeholder="Retype password" autocomplete="off" required="required" data-rule-equalto="#password" data-msg-equalto="Please enter the same value again.">
+						</div>
+					</div>
+				</div>
+				<div class="panel-footer">
+					<button type="submit" class="btn btn-primary"><span class="fa fa-check-circle"></span> Change Password</button>
+				</div>
+			</div>
+		</form>
+	</div>
+</div>
+@endif
 	</div>
 	</div>
 	</div>
